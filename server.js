72ca31343d21fccabd3703cd6bdc422c7f6e8815
Abha Gupta <agupta3@walmart.com>
2016-04-11 13:58:55 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    res.sendfile('index.html')
+ });
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
+});
